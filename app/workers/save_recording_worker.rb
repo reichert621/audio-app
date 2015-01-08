9583147f1d6a8b03dc845ae41fbee26afbc97a7b
Alex Reichert <alex@graphscience.com>
2015-01-07 23:58:35 -0800

    Trying new approach to saving recordings

--- app/workers/save_recording_worker.rb
+++ app/workers/save_recording_worker.rb
@@ -1,23 +1,12 @@
 class SaveRecordingWorker
-  require 'base64'
   include Sidekiq::Worker
   # sidekiq_options queue: "high"
   sidekiq_options retry: 3
   
-  def perform(recording_id, audio_file)
+  def perform(recording_id)
     recording = Recording.find(recording_id)
-    decoded_audio = Base64.decode64(audio_file.split(",").last)
-    file = File.open("#{Rails.root}/tmp/#{recording.name}", "wb:binary") do |f|
-      f.write(decoded_audio)
-    end
-
     recording.audio = File.open "#{Rails.root}/tmp/#{recording.name}"
-
-    begin
-      recording.save!
-    rescue => e
-      raise e
-    end
+    recording.save!
   end
 
 end
