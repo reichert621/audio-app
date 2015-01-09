be835c9765ed48265e019f05e77a135deeb0761a
Alex Reichert <alex@graphscience.com>
2015-01-08 17:26:09 -0800

    Add options/auth to Dalli Client

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,4 +3,12 @@ url = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
 REDIS = Redis.new(url: url)
 
 servers = (ENV["MEMCACHIER_SERVERS"] || "localhost:11211").split(",")
-MEMCACHED = ConnectionPool.new(size: 5, timeout: 5) { Dalli::Client.new(servers, { compress: true }) }
+options = {
+  :username => ENV["MEMCACHIER_USERNAME"],
+  :password => ENV["MEMCACHIER_PASSWORD"],
+  :failover => true,
+  :socket_timeout => 1.5,
+  :socket_failure_delay => 0.2,
+  compress: true
+}
+MEMCACHED = ConnectionPool.new(size: 5, timeout: 5) { Dalli::Client.new(servers, options) }
