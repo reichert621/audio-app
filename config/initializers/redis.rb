5b3751fa4a89a0223d4b7e61c8bd8805a6d3e366
Alex Reichert <alex@graphscience.com>
2015-01-08 17:13:55 -0800

    Attempt to fix memcachier server for heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,4 +2,5 @@
 url = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
 REDIS = Redis.new(url: url)
 
-MEMCACHED = ConnectionPool.new(size: 5, timeout: 5) { Dalli::Client.new('localhost:11211', { compress: true }) }
+servers = (ENV["MEMCACHIER_SERVERS"] || "localhost:11211").split(",")
+MEMCACHED = ConnectionPool.new(size: 5, timeout: 5) { Dalli::Client.new(servers, { compress: true }) }
